0fcc5fb939facd94054d4b0b15c4837db040a7f3
yTanaka <pirori.nyanko.033014@gmail.com>
2018-06-04 10:06:50 +0900

    [fix]プレビュー画面が出るように修正 最小SDKを別のバージョンに変えたら表示できた その後元に戻しても問題なし

--- app/src/main/java/com/example/eccprog/dailyreport/MainActivity.kt
+++ app/src/main/java/com/example/eccprog/dailyreport/MainActivity.kt
@@ -2,11 +2,38 @@ package com.example.eccprog.dailyreport
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val teamNameSpinner : Spinner = findViewById(R.id.teamNameSpinner)  // チーム名のトグルのやつ
+        val radioGroup : RadioGroup = findViewById(R.id.teamGroup)  // ラジオボタン
+
+        // 新規作成画面 … 個人のほうにチェック入れておく
+        // 編集画面     … 初期値を読み込んでどちらかのラジオボタンにチェックを入れる
+        val onlyButton : RadioButton = findViewById(R.id.onlyButton)
+        onlyButton.isChecked = true
+        teamNameSpinner.isClickable = false
+
+        // ラジオボタンがチームを指しているときはスピナー(チーム名一覧)を有効化
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            val selectedRadioButton : RadioButton = findViewById(checkedId) // 選択中のラジオボタン
+            teamNameSpinner.isClickable = ( selectedRadioButton == findViewById(R.id.teamButton) )
+        }
+
+//        fun onCreateOptionsMenu(menu : Menu) : Boolean {
+//            menuInflater.inflate(R.menu.menu_writer, menu)
+//            return super.onCreateOptionsMenu(menu)
+//        }
+
+
     }
 }
